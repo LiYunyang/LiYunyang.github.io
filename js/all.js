9383ada05bf2ae97e7473825536b8a5f688f795e
YL <20972148+LiYunyang@users.noreply.github.com>
2023-12-11 09:23:39 -0500

    [UPDATE] intro

--- js/all.js
+++ js/all.js
@@ -217,8 +217,7 @@ function insert_stats_tracker(){
             var sc_invisible=0; 
             var sc_security="b5b94723"; 
             var scJsHost = "https://";
-            document.write("<sc"+"ript type='text/javascript' src='" +
-            scJsHost+"statcounter.com/counter/counter.js'></"+"script>");
+            document.write("<sc"+"ript type='text/javascript' src='" + scJsHost+"statcounter.com/counter/counter.js'></"+"script>");
         </script>
        <!-- End of Statcounter Code -->`
     document.body.insertAdjacentHTML('beforeend', statCounterCode);
